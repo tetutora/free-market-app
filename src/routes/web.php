67c43cfb353f1603e0c5d23347a17dd4bc34faa9
kitano <beiyelinai01@gmail.com>
2025-05-22 17:15:55 +0900

    ユーザー認証（メール認証含む）実装完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+})->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/logout', function () {
-    Auth::logout();
-    return redirect('/');
-});
+require __DIR__.'/auth.php';
